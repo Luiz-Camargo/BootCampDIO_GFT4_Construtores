24cb970a55b8ca6b211f906bbf1cb037f89401da
Luiz Henrique Alberto de Camargo <71042652+Luiz-Camargo@users.noreply.github.com>
2022-03-02 15:00:37 -0300

    first commit

---
+++ ExemploConstrutores/Models/Pessoa.cs
@@ -0,0 +1,26 @@
+namespace ExemploConstrutores.Models
+{
+    public class Pessoa
+    {
+        private readonly string nome = "Luiz";
+        private readonly string sobrenome;
+
+        // public Pessoa()
+        // {
+            // nome = string.Empty;
+            // sobrenome = string.Empty;
+        // }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            Console.WriteLine("Construtor classe pessoa!");
+        }
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é {nome} {sobrenome}");
+        }
+    }
+}
---
+++ ExemploConstrutores/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+
+        public delegate void Operacao(int x, int y);
+
+        static void Main(string[] args)
+        {
+            // Operacao op = Calculadora.Somar;
+            // op(10,10);
+
+            // Operacao op = new Operacao(Calculadora.Somar);
+            // op += Calculadora.Subtrair;
+            // op.Invoke(10,10);
+
+            // Operacao op2 = new Operacao(Calculadora.Subtrair);
+            // op2.Invoke(50,25);
+            
+            // const double pi = 3.14;
+ 
+            // Console.WriteLine(pi);
+
+            // Log log = Log.GetInstance();
+
+            // log.PropriedadeLog = "Teste instancia";
+
+            // Log log2 = Log.GetInstance();
+            // Console.WriteLine(log2.PropriedadeLog);
+
+
+            // Pessoa p1 = new Pessoa("Luiz Henrique", "Alberto de Camargo");
+            // p1.Apresentar();
+
+            // Aluno a1 = new Aluno("Luiz Henrique", "Alberto de Camargo", "Programação Orientada a Objetos");
+            // a1.Apresentar();
+
+            // Data data = new Data();
+            // data.SetMes(20);
+
+            // data.Mes = 12;
+            // Console.WriteLine(data.Mes);
+
+            // data.ApresentarMes();
+        }
+    }
+}
